154b6bc1e17e984acb5c2b2d70eb56b9e4a75b14
Jacob Walser <jwalser90@gmail.com>
2021-03-18 22:18:53 -0400

    flash_px4.py: use new uploader.py tool from ardupilot

--- tools/flash_px4.py
+++ tools/flash_px4.py
@@ -74,14 +74,12 @@ os.system("screen -X -S mavproxy quit")
 # Flash Pixhawk
 print "Flashing Pixhawk..."
 if options.file is not None:
-    if(os.system("python -u " + home + "/companion/tools/px_uploader.py --port "
-    "/dev/serial/by-id/usb-3D_Robotics_PX4_BL_FMU_v2.x_0-if00,/dev/autopilot "
+    if(os.system("python -u " + home + "/companion/tools/uploader.py "
     "'%s'" % options.file) != 0):
                 print "Error flashing pixhawk!"
                 exit(1)
 else:
-    if(os.system("python -u "+home+"/companion/tools/px_uploader.py --port "
-    "/dev/serial/by-id/usb-3D_Robotics_PX4_BL_FMU_v2.x_0-if00,/dev/autopilot "
+    if(os.system("python -u "+home+"/companion/tools/uploader.py "
     "/tmp/ardusub.apj") != 0):
                 print "Error flashing pixhawk! Do you have most recent version of companion? Try 'git pull' or scp."
                 exit(1)
